007753a1e8da5960701d300409795d3015645777
Miguel <diazmiiguel@gmail.com>
2017-06-07 23:51:14 -0400

    Adds another worker

--- lib/godfist/application.ex
+++ lib/godfist/application.ex
@@ -5,7 +5,8 @@ defmodule Godfist.Application do
     import Supervisor.Spec
 
     children = [
-      worker(Cachex, [:id_cache, [default_ttl: 86_400_000], []])
+      worker(Cachex, [:id_cache, [default_ttl: 86_400_000], []], id: 1),
+      worker(Cachex, [:summid_cache, [default_ttl: 86_400_000], []], id: 2)
     ]
 
     Supervisor.start_link(children, strategy: :one_for_all)
