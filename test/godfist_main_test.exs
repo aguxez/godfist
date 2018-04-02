af654ea11d5db53e04a113d814d04cf6a7cf5e44
aguxez <dizmiiguel@gmail.com>
2018-04-02 01:24:38 -0400

    test

--- test/godfist_main_test.exs
+++ test/godfist_main_test.exs
@@ -1,41 +1,89 @@
 defmodule Godfist.MainTest do
   @moduledoc false
 
-  use ExUnit.Case
+  use ExUnit.Case, async: true
 
-  test "return account id and summoner id of player" do
-    {:ok, account_id} = Godfist.get_account_id(:lan, "Mjólner")
+  alias Godfist.LeagueRates
+
+  setup do
+    bypass = Bypass.open()
+    LeagueRates.start_link(bypass.port)
+
+    {:ok, bypass: bypass}
+  end
+
+  test "return champs with similar name to the query", %{bypass: bypass} do
+    Bypass.expect(bypass, fn conn ->
+      Plug.Conn.resp(conn, 200, ~s<{"Zoe": {"name": "Zoe"}}>)
+    end)
+
+    similars =
+      "Zo"
+      |> Godfist.find_similar()
+      |> Enum.map(fn {key, _v} -> key end)
+
+    assert similars == ["Zoe"]
+  end
+
+  test "return account id of player", %{bypass: bypass} do
+    Bypass.expect(bypass, fn conn ->
+      Plug.Conn.resp(conn, 200, ~s<{"accountId": 200008206}>)
+    end)
+
+    {:ok, account_id} = Godfist.get_account_id(:lan, "Aguxez")
     assert 200_008_206 == account_id
+  end
 
-    {:ok, summoner_id} = Godfist.get_summid(:lan, "Mjólner")
+  test "returns summoner_id of player", %{bypass: bypass} do
+    Bypass.expect(bypass, fn conn ->
+      Plug.Conn.resp(conn, 200, ~s<{"id": 24244}>)
+    end)
+
+    {:ok, summoner_id} = Godfist.get_summid(:lan, "Aguxez")
     assert 24_244 == summoner_id
   end
 
-  test "return matchlist" do
-    {:ok, matches} = Godfist.matchlist(:lan, "Mjólner", champion: 64)
+  test "returns matchlist", %{bypass: bypass} do
+    Bypass.expect(bypass, fn conn ->
+      Plug.Conn.resp(conn, 200, ~s<{"endIndex": 100}>)
+    end)
 
-    assert is_map(matches) == true
+    action = Godfist.matchlist(:lan, "Aguxez", champion: 64)
+
+    assert {:ok, data} = action
+    assert data["endIndex"] == 100
   end
 
   # Can't test active game right now because of it would fail if there is not an
   # active match.
 
-  test "champion tests" do
+  test "all_champs tests", %{bypass: bypass} do
+    Bypass.expect(bypass, fn conn ->
+      Plug.Conn.resp(conn, 200, ~s<{}>)
+    end)
+
     {:ok, all_champs} = Godfist.all_champs(:na, ftp: true)
-    {:ok, champ} = Godfist.champion(:ru, 64)
-    {name, _map} = Godfist.champion_by_name("Kha'Zix")
 
     assert is_map(all_champs) == true
-    assert is_map(champ) == true
-    assert name == "Khazix"
   end
 
-  test "return champs with similar name to the query" do
-    similars =
-      "Ta"
-      |> Godfist.find_similar()
-      |> Enum.map(fn {k, _v} -> k end)
+  test "champion_by_name", %{bypass: bypass} do
+    Bypass.expect(bypass, fn conn ->
+      Plug.Conn.resp(conn, 200, ~s<{"data": {"Talon": {"name": "Talon"}}}>)
+    end)
 
-    assert similars == ["Talon", "TahmKench", "Taric", "Taliyah"]
+    {name, _map} = Godfist.champion_by_name("Talon")
+
+    assert name == "Talon"
+  end
+
+  test "champion test", %{bypass: bypass} do
+    Bypass.expect(bypass, fn conn ->
+      Plug.Conn.resp(conn, 200, ~s<{"id": 64}>)
+    end)
+
+    {:ok, champ} = Godfist.champion(:ru, 64)
+
+    assert champ["id"] == 64
   end
 end
