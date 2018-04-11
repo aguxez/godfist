cb3bf48e786f0b3409137de060e9d27c2bd808ac
aguxez <dizmiiguel@gmail.com>
2018-04-10 21:40:00 -0400

    added another cache worker for static champs data

--- lib/godfist/application.ex
+++ lib/godfist/application.ex
@@ -22,7 +22,8 @@ defmodule Godfist.Application do
       worker(Cachex, [:id_cache, [default_ttl: 86_400_000], []], id: 1),
       worker(Cachex, [:summid_cache, [default_ttl: 86_400_000], []], id: 2),
       worker(Cachex, [:champion, [default_ttl: 86_400_000], []], id: 3),
-      worker(Cachex, [:all_champs, [default_ttl: 14_400_000], []], id: 4)
+      worker(Cachex, [:all_champs, [default_ttl: 14_400_000], []], id: 4),
+      worker(Cachex, [:static_champs, [default_ttl: 86_400_000], []], id: 5)
     ]
   end
 end
